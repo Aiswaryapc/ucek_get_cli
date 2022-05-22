242579e0b6521f9c4a407d3b0bb1388d974b2aba
Aiswaryapc <aiswaryapchandran3@gmail.com>
2022-05-23 01:04:12 +0530

    auth

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,11 @@
 import 'package:get/get.dart';
 
+import '../modules/authentication/bindings/authentication_binding.dart';
+import '../modules/authentication/login/bindings/login_binding.dart';
+import '../modules/authentication/login/views/login_view.dart';
+import '../modules/authentication/sign_up/bindings/sign_up_binding.dart';
+import '../modules/authentication/sign_up/views/sign_up_view.dart';
+import '../modules/authentication/views/authentication_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 
@@ -15,6 +21,23 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTHENTICATION,
+      page: () => AuthenticationView(),
+      binding: AuthenticationBinding(),
+      children: [
+        GetPage(
+          name: _Paths.LOGIN,
+          page: () => LoginView(),
+          binding: LoginBinding(),
+        ),
+        GetPage(
+          name: _Paths.SIGN_UP,
+          page: () => SignUpView(),
+          binding: SignUpBinding(),
+        ),
+      ],
     ),
   ];
 }
