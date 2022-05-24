49e7644f612493fed30d713f95b1340bc150bd5b
HIran M <45740292+hiranmohanan@users.noreply.github.com>
2022-05-24 19:04:18 +0530

    first view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:get/get.dart';
+import 'package:ucek/app/widget/theme/colors.dart';
+import 'package:ucek/app/widget/theme/string_const.dart';
+import 'package:ucek/app/widget/theme/text_theme.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,14 +13,94 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        title: Text(
+          "hi " + AppStrings.fname,
+          style: TextStyle(color: AppColors.black),
         ),
+        actions: [
+          Row(
+            children: [
+              Icon(
+                Icons.microwave_outlined,
+                color: AppColors.black,
+              ),
+            ],
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Material(
+            child: Expanded(
+          child: Padding(
+            padding: EdgeInsets.only(
+                top: Get.height * 0.1,
+                left: Get.width * 0.05,
+                right: Get.width * 0.05,
+                bottom: Get.height * 0.2),
+            child: Container(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: Get.height * 0.04,
+                mainAxisSpacing: Get.height * 0.04,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: AppColors.slimyGreen,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: AppColors.buttonBlue,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: AppColors.fireOpalRed,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: AppColors.selectiveYellow,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: AppColors.white,
+        selectedItemColor: AppColors.chineseSilver,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.edit_calendar_outlined,
+              //color: AppColors.buttonBlue,
+            ),
+            label: AppStrings.attendence,
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.edit_calendar_outlined,
+                //color: AppColors.buttonBlue,
+              ),
+              label: AppStrings.studlist),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.edit_calendar_outlined,
+                // color: AppColors.white,
+              ),
+              label: AppStrings.schedule)
+        ],
+        backgroundColor: AppColors.mediumElectricBlue,
       ),
     );
   }
